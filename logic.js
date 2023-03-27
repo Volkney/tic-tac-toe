4f8159d13d00c16a3a37cc60f142cfefbf91a8b3
Jorge Tinoco <volkney@Jorges-MacBook-Air.local>
2023-03-27 17:02:24 -0500

    changed reset function to reload page when play again button is pressed

--- logic.js
+++ logic.js
@@ -30,10 +30,10 @@ const Player = (symbol, playerNumber, player1, player2) => {
       }
       if(currentPlayer === player1){
         currentPlayer = player2;
-        choiceDisplay.textContent = `Current Player: ${currentPlayer.getNumber()}`;
+        choiceDisplay.textContent = `Current Player: ${currentPlayer.getSymbol()}`;
       } else {
         currentPlayer = player1;
-        choiceDisplay.textContent = `Current Player: ${currentPlayer.getNumber()}`;
+        choiceDisplay.textContent = `Current Player: ${currentPlayer.getSymbol()}`;
       }
     };
     //third module
@@ -47,7 +47,8 @@ const Player = (symbol, playerNumber, player1, player2) => {
           const handleClick = (e) => {
             const clickedGrid = e.target.dataset.value;
             if (!gameEnded) {
-              for (let i = 0; i < board.length; i++) {              
+              for (let i = 0; i < board.length; i++) {   
+                           
                 if (clickedGrid === board[i] && indices[i].textContent === '') {
                   console.log(`this is clicknumber ${i}`);//this could be use as a turn counter
                   indices[i].textContent = currentPlayer.getSymbol();
@@ -85,8 +86,8 @@ const Player = (symbol, playerNumber, player1, player2) => {
       const [a, b, c] = winningIndices[i];
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
         // declare the winner
-        console.log(`Player ${currentPlayer.getNumber()} wins!`);
-        winnerDisplay.textContent = `Player ${currentPlayer.getNumber()} wins!`;
+        console.log(`${currentPlayer.getNumber()} wins!`);
+        winnerDisplay.textContent = `${currentPlayer.getNumber()} wins!`;
         choiceDisplay.style.display = 'none';
         gameEnded = true;
         return;
@@ -110,34 +111,13 @@ const Player = (symbol, playerNumber, player1, player2) => {
   };
 
   //sixth module
-  const resetBoard = (board) => {
+  const resetBoard = () => {
     playAgain.addEventListener('click', () => {
-      // clear the board
-      board = getBoard();
-
-      // clear the board
-      indices.forEach(element => {
-        element.textContent = '';
-      });
-      // clear the board
-      board.forEach(element => {
-        board.textContent = '';
-      });
-      
-      // clear the winner display
-      winnerDisplay.textContent = '';
-      // enable the player choice buttons
-      playerChoice.forEach(choice => {
-        choice.disabled = false;
-      });
-      // reset the current player display
-      choiceDisplay.textContent = 'Choose X or O to start the game';
-      //reset moves
-      movesMade = 0;
-      // reset the gameEnded variable
-      gameEnded = false;
+      location.reload();
     });
   };
+  
+  
 
   updateBoard();  //for this to run the whole modules
   resetBoard();
@@ -168,14 +148,14 @@ function getPlayersChoice() {
     choice.addEventListener('click', e => {
 
       const indexValue = e.target.value;
-      if (indexValue === 'x') {
+      if (indexValue === 'X') {
         player1 = Player(indexValue, 'Player 1');
-        player2 = Player('o', 'Player 2');
+        player2 = Player('O', 'Player 2');
         player1.Game(player1, player2); // call Game function with player1 and player2 objects as arguments
         playerChoice[1].disabled = true;
       } else {
         player1 = Player(indexValue, 'Player 1');
-        player2 = Player('x', 'Player 2');
+        player2 = Player('X', 'Player 2');
         player2.Game(player1, player2); // call Game function with player1 and player2 objects as arguments
         playerChoice[0].disabled = true;
       }
